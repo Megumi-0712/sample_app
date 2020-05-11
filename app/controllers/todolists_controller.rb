407285c2a6c14d8d14421819a32db506903a4893
Megumi-0712 <toruansya@yahoo.co.jp>
2020-05-11 15:17:21 +0900

    [start]3-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -16,10 +16,13 @@ class TodolistsController < ApplicationController
     def index
         @lists = List.all
     end
-
     def show
         @list = List.find(params[:id])
     end
+    def edit
+        @list = List.find(params[:id])
+    end
+
 
 
     private
