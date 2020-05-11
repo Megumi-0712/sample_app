c288efece3e890348cd263505d912d14afceaf86
Megumi-0712 <toruansya@yahoo.co.jp>
2020-05-11 17:52:16 +0900

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd43d8d83386cbf692f821e68bbd2a747ef9ae4579b3170af17057e7d46f9e69b2989f169b6bf44bad9ec373fcb68abf33d91e879bf57a5feb511c486fd078ea1'
